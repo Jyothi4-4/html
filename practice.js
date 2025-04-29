4e43fd18466dea671a0ed7de79ef5900471faf42
Jyothi4-4 <mandajyothireddy26@gmail.com>
2025-04-29 17:18:27 +0530

    Learning Html

---
+++ practice.js
@@ -0,0 +1,18 @@
+//var  Stvring = "kriti"
+
+//console.log(String)
+//var String="jyothi"
+//console.log(String)
+
+//let language ="python";
+//console.log(language)
+ language = "html"
+console.log(language)
+ 
+//const language = "python"
+//console.log(language)
+ //language = "css"
+//console.log(language)
+ 
+ 
+
